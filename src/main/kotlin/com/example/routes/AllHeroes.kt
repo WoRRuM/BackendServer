741d6ca2153f26f96192039a45fd73917a362d18
worrum <yigoninwork@gmail.com>
2022-10-14 23:52:59 +0300

    Backend Repository

---
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -0,0 +1,42 @@
+package com.example.routes
+
+import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
+fun Route.getAllHeroes() {
+
+    val heroRepository: HeroRepository by inject()
+
+
+    get("/boruto/heroes") {
+        try {
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            require(page in 1..5)
+
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
+        } catch (e: NumberFormatException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Only Numbers Allowed."),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Heroes Not Found."),
+                status = HttpStatusCode.NotFound
+            )
+
+        }
+    }
+}
